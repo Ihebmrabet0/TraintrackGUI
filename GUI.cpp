fb4b9f8ef83b9729e8556be169d8a2c75fe7b857
Ihebmrabet0 <145569186+Ihebmrabet0@users.noreply.github.com>
2024-01-10 14:51:56 +0100

    fix: slider

--- GUI.cpp
+++ GUI.cpp
@@ -47,33 +47,33 @@ int main(int argc, char *argv[]) {
     // Start Button
     QPushButton *startButton2 = new QPushButton("START");
     startButton2->setStyleSheet(buttonStyle);
-    gridLayout->addWidget(startButton2, 2, 0, 1, 4);
+    gridLayout->addWidget(startButton2, 3, 0, 1, 4);
 
     // Track selection buttons
     QPushButton *trackOneButton = new QPushButton("1.Track");
     QPushButton *trackTwoButton = new QPushButton("2.Track");
     trackOneButton->setStyleSheet(buttonStyle);
     trackTwoButton->setStyleSheet(buttonStyle);
-    gridLayout->addWidget(trackOneButton, 3, 0, 1, 2);
-    gridLayout->addWidget(trackTwoButton, 3, 2, 1, 2);
+    gridLayout->addWidget(trackOneButton, 4, 0, 1, 2);
+    gridLayout->addWidget(trackTwoButton, 4, 2, 1, 2);
 
     // Working time display
     QLabel *workingTimeLabel = new QLabel("12:30\nWorking Time");
     workingTimeLabel->setStyleSheet(labelStyle);
-    gridLayout->addWidget(workingTimeLabel, 4, 0, 1, 2);
+    gridLayout->addWidget(workingTimeLabel, 5, 0, 1, 2);
 
     // Laps display
     QLabel *lapsLabel = new QLabel("003\nLaps");
     lapsLabel->setStyleSheet(labelStyle);
-    gridLayout->addWidget(lapsLabel, 4, 2, 1, 2);
+    gridLayout->addWidget(lapsLabel, 5, 2, 1, 2);
 
     // Additional buttons
     QPushButton *lightButton = new QPushButton("Light");
     QPushButton *moreSettingsButton = new QPushButton("More Settings");
     lightButton->setStyleSheet(buttonStyle);
     moreSettingsButton->setStyleSheet(buttonStyle);
-    gridLayout->addWidget(lightButton, 5, 0, 1, 2);
-    gridLayout->addWidget(moreSettingsButton, 5, 2, 1, 2);
+    gridLayout->addWidget(lightButton, 6, 0, 1, 2);
+    gridLayout->addWidget(moreSettingsButton, 6, 2, 1, 2);
 
     // Set central widget and show main window
     mainWindow.setCentralWidget(centralWidget);
