581711f50182552c51a085723fe84f3e916f3944
Ihebmrabet0 <145569186+Ihebmrabet0@users.noreply.github.com>
2024-01-10 14:49:20 +0100

    Add train2

--- GUI.cpp
+++ GUI.cpp
@@ -23,61 +23,61 @@ int main(int argc, char *argv[]) {
     
     // Train 1
     // Speed Slider
-    QSlider *speedSlider = new QSlider(Qt::Horizontal);
-    speedSlider->setRange(0, 100);
-    QLabel *speedLabel = new QLabel("Speed");
-    speedLabel->setStyleSheet(labelStyle);
-    gridLayout->addWidget(speedLabel, 0, 0, 1, 1);
-    gridLayout->addWidget(speedSlider, 0, 1, 1, 3);
+    QSlider *speedSlider1 = new QSlider(Qt::Horizontal);
+    speedSlider1->setRange(0, 100);
+    QLabel *speedLabel1 = new QLabel("Speed");
+    speedLabel1->setStyleSheet(labelStyle);
+    gridLayout->addWidget(speedLabel1, 0, 0, 1, 1);
+    gridLayout->addWidget(speedSlider1, 0, 1, 1, 3);
 
     // Start Button
-    QPushButton *startButton = new QPushButton("START");
-    startButton->setStyleSheet(buttonStyle);
-    gridLayout->addWidget(startButton, 1, 0, 1, 4);
+    QPushButton *startButton1 = new QPushButton("START");
+    startButton1->setStyleSheet(buttonStyle);
+    gridLayout->addWidget(startButton1, 1, 0, 1, 4);
 
     // Train 2
     // Speed Slider
-    QSlider *speedSlider = new QSlider(Qt::Horizontal);
-    speedSlider->setRange(0, 100);
-    QLabel *speedLabel = new QLabel("Speed");
-    speedLabel->setStyleSheet(labelStyle);
-    gridLayout->addWidget(speedLabel, 0, 0, 1, 1);
-    gridLayout->addWidget(speedSlider, 0, 1, 1, 3);
+    QSlider *speedSlider2 = new QSlider(Qt::Horizontal);
+    speedSlider2->setRange(0, 100);
+    QLabel *speedLabel2 = new QLabel("Speed");
+    speedLabel2->setStyleSheet(labelStyle);
+    gridLayout->addWidget(speedLabel2, 2, 0, 1, 1);
+    gridLayout->addWidget(speedSlider2, 2, 1, 1, 3);
 
     // Start Button
-    QPushButton *startButton = new QPushButton("START");
-    startButton->setStyleSheet(buttonStyle);
-    gridLayout->addWidget(startButton, 1, 0, 1, 4);
+    QPushButton *startButton2 = new QPushButton("START");
+    startButton2->setStyleSheet(buttonStyle);
+    gridLayout->addWidget(startButton2, 2, 0, 1, 4);
 
     // Track selection buttons
     QPushButton *trackOneButton = new QPushButton("1.Track");
     QPushButton *trackTwoButton = new QPushButton("2.Track");
     trackOneButton->setStyleSheet(buttonStyle);
     trackTwoButton->setStyleSheet(buttonStyle);
-    gridLayout->addWidget(trackOneButton, 2, 0, 1, 2);
-    gridLayout->addWidget(trackTwoButton, 2, 2, 1, 2);
+    gridLayout->addWidget(trackOneButton, 3, 0, 1, 2);
+    gridLayout->addWidget(trackTwoButton, 3, 2, 1, 2);
 
     // Working time display
     QLabel *workingTimeLabel = new QLabel("12:30\nWorking Time");
     workingTimeLabel->setStyleSheet(labelStyle);
-    gridLayout->addWidget(workingTimeLabel, 3, 0, 1, 2);
+    gridLayout->addWidget(workingTimeLabel, 4, 0, 1, 2);
 
     // Laps display
     QLabel *lapsLabel = new QLabel("003\nLaps");
     lapsLabel->setStyleSheet(labelStyle);
-    gridLayout->addWidget(lapsLabel, 3, 2, 1, 2);
+    gridLayout->addWidget(lapsLabel, 4, 2, 1, 2);
 
     // Additional buttons
     QPushButton *lightButton = new QPushButton("Light");
     QPushButton *moreSettingsButton = new QPushButton("More Settings");
     lightButton->setStyleSheet(buttonStyle);
     moreSettingsButton->setStyleSheet(buttonStyle);
-    gridLayout->addWidget(lightButton, 4, 0, 1, 2);
-    gridLayout->addWidget(moreSettingsButton, 4, 2, 1, 2);
+    gridLayout->addWidget(lightButton, 5, 0, 1, 2);
+    gridLayout->addWidget(moreSettingsButton, 5, 2, 1, 2);
 
     // Set central widget and show main window
     mainWindow.setCentralWidget(centralWidget);
-    mainWindow.resize(400, 300);
+    mainWindow.resize(400, 380);
     mainWindow.show();
 
     return app.exec();
